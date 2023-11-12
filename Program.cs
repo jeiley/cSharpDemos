bf02596a11179968ab01fd0dfa134c909665f562
Melek <115369650+jeiley@users.noreply.github.com>
2023-11-12 18:04:13 +0300

    0

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+namespace vars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num= 2;
+            string datetime = DateTime.Now.ToString("dd.MM.yyyy");
+            string dateTime = DateTime.Now.ToString("dd/MM/yyyy");
+            string hour = DateTime.Now.ToString("HH:mm");
+            Console.WriteLine(num);
+            Console.WriteLine(datetime);
+            Console.WriteLine(hour);
+
+            Console.ReadLine();
+
+        }
+    }
+}
